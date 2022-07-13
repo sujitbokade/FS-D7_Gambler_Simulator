83336b89d6e32f13d3ec11a9299f1051303576f7
sujit <sujitbokade2014@gmail.com>
2022-07-13 21:58:30 +0530

    Calculative Gambler if won or lost 50% of the stake

--- src/com/bridgelabz/GamblerSimulator.java
+++ src/com/bridgelabz/GamblerSimulator.java
@@ -1,9 +1,12 @@
 package com.bridgelabz;
 
+import java.util.Date;
+
 public class GamblerSimulator {
     static int DAILY_STAKE = 100;
     static int BET_PER_GAME = 1;
     static int win=1;
+
     static void winCheck(){
         int result=(int)(Math.random()*10)%2;
         if(result==win){
@@ -16,8 +19,14 @@ public class GamblerSimulator {
         }
         System.out.println("Daily Stake = "+DAILY_STAKE);
     }
+
+         static void resignDayCheck(){
+          while (DAILY_STAKE<150 && DAILY_STAKE>50){
+              winCheck();
+          }
+         }
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator!");
-        winCheck();
+        resignDayCheck();
     }
 }
