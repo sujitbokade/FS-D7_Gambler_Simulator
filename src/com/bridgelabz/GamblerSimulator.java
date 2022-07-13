bda7e64e12ac70de37268b807084e871edd1b9b1
sujit <sujitbokade2014@gmail.com>
2022-07-13 21:16:00 +0530

    Initial Commit

---
+++ src/com/bridgelabz/GamblerSimulator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class GamblerSimulator {
+    static int DAILY_STAKE = 100;
+    static int BET_PER_GAME = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator!");
+    }
+}
