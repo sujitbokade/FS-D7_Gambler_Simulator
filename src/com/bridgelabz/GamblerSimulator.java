69bf5bf8fff58de3a78898f46f8d05b33cf0e3e9
sujit <sujitbokade2014@gmail.com>
2022-07-15 17:29:59 +0530

    Maximum Won & Lost day

--- src/com/bridgelabz/GamblerSimulator.java
+++ src/com/bridgelabz/GamblerSimulator.java
@@ -1,51 +1,66 @@
 package com.bridgelabz;
 
-import java.util.Date;
+
 
 public class GamblerSimulator {
     static int DAILY_STAKE = 100;
-    static int BET_PER_GAME = 1;
+
     static int win = 1;
     static int totalAmountWon = 0;
     static int totalAmountLost = 0;
-    static int dayswon = 0;
-    static int dayslost = 0;
-
+    static int daysWon = 0;
+    static int daysLost = 0;
+    static int luckyDay;
+    static int unluckyDay;
+    static int totalStake;
+    static int maxWin=0;
+    static int maxLost=0;
 
     static void winCheck() {
         int result = (int) (Math.random() * 10) % 2;
         if (result == win) {
-            DAILY_STAKE++;
+            totalStake++;
         } else {
-            DAILY_STAKE--;
+            totalStake--;
         }
     }
 
-    static void resignDayCheck() {
+    static void maxWinLostCheck() {
         for (int day = 1; day <= 20; day++) {
-            DAILY_STAKE = 100;
-            while (DAILY_STAKE < 150 && DAILY_STAKE > 50) {
+            totalStake=DAILY_STAKE;
+            while (totalStake< 150 && totalStake > 50) {
                 winCheck();
             }
-            if (DAILY_STAKE > 100) {
+            if (totalStake > 100) {
                 totalAmountWon += 50;
-                System.out.println("won on day " + day + " is " + (DAILY_STAKE - 100));
-                dayswon++;
-            } else {
+                if(totalStake>maxWin){
+                    maxWin=totalStake;
+                    luckyDay=day;
+                }
+                System.out.println("won on day " + day + " is " + (totalStake - 100));
+                daysWon++;
+            }
+            else {
                 totalAmountLost -= 50;
-                System.out.println("Lost on day " + day + " is " + (100 - DAILY_STAKE));
-                dayslost++;
+                if(totalStake>maxLost){
+                    maxLost=totalStake;
+                    unluckyDay=day;
+                }
+                System.out.println("Lost on day " + day + " is " + (100 - totalStake));
+                daysLost++;
             }
         }
             System.out.println("Total Amount Won in 20 Days " + totalAmountWon);
             System.out.println("Total Amount Lost in 20 Days " + totalAmountLost);
-            System.out.println("No of Days Won " + dayswon);
-            System.out.println("No of Days Lost " + dayslost);
+            System.out.println("No of Days Won " + daysWon);
+            System.out.println("No of Days Lost " + daysLost);
+            System.out.println("lucky day is "+luckyDay);
+            System.out.println("unlucky day is "+unluckyDay);
 
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator!");
-        resignDayCheck();
+        maxWinLostCheck();
     }
 }
